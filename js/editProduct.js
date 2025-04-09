ea6eb570ca59f857c4fdfa225df5032c42ef26e0
jacobstxt <153942144+jacobstxt@users.noreply.github.com>
2025-04-09 22:17:58 +0300

    Now I get info about my product at editProduct.html

---
+++ js/editProduct.js
@@ -0,0 +1,35 @@
+﻿const ProductForm = document.getElementById('createProductForm');
+
+
+
+window.addEventListener('load', async () => {
+    const id = new URLSearchParams(window.location.search).get('id') || null;
+    console.log("id", id);
+
+    const response = await axios.get(`${window.API_BASE_URL}/api/Products/get/${id}`, {
+        headers: {
+            'Accept': '*/*'
+        }
+    });
+
+    const { data } = response;
+    console.log("Дані", data);
+
+
+    document.getElementById('ProductTitle').value = data.name;
+    document.getElementById('ProductCategory').value = data.categoryName;
+    document.getElementById('ProductPriority').value = data.priority;
+    document.getElementById('ProductPrice').value = data.price;
+    tinymce.get("ProductDescription").setContent(data.description);
+
+    const imagesContainer = document.getElementById('ProductImagesList');
+    imagesContainer.innerHTML = "";
+
+    data.images.foreach(img => {
+        const imageElement = document.createElement("img");
+        imageElement.src = `${window.API_BASE_URL}/images/100_${img.name}`;
+        imageElement.alt = "Product image";
+        imageElement.className = "h-32 rounded shadow";
+        imagesContainer.appendChild(imageElement);
+    });
+});
