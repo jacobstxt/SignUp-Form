1db02c0d4a44884f3ce090e61995be10dbc7216c
jacobstxt <153942144+jacobstxt@users.noreply.github.com>
2025-04-10 20:13:45 +0300

    Create Edit for Products but photos  don't work properly

--- js/editProduct.js
+++ js/editProduct.js
@@ -1,6 +1,63 @@
 ﻿const ProductForm = document.getElementById('createProductForm');
 
 
+ProductForm.onsubmit = (e) => {
+    e.preventDefault();
+
+    const imageElements = document.querySelectorAll('#ProductImagesList img');
+    let image = null;
+
+    imageElements.forEach(img => {
+        if (img.src.startsWith("data:")) {
+            image = img.src; 
+        }
+    });
+
+    const xhr = new XMLHttpRequest();
+
+    const data = {
+        id: new URLSearchParams(window.location.search).get('id') || null,
+        name: document.getElementById("ProductTitle").value,
+        priority: document.getElementById("ProductPriority").value,
+        categoryId: parseInt(document.getElementById("ProductCategory").value),
+        price: document.getElementById("ProductPrice").value,
+        description: tinymce.get("ProductDescription").getContent(),
+        image: image, 
+        ids: [] 
+    };
+
+
+    console.log("info", data);
+
+    const url = `${window.API_BASE_URL}/api/Products/edit`;
+    show_loading();
+    xhr.open("PUT", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    hide_loading();
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                const resp = xhr.responseText;
+                location.href = "/partials/productsList.html";
+            } else {
+                HandleError(xhr.responseText);
+            }
+        }
+    };
+
+    xhr.send(JSON.stringify(data));
+}
+
+
+
+function HandleError(responseText) {
+    console.log(responseText);
+}
+
+
+
+
 
 window.addEventListener('load', async () => {
     const id = new URLSearchParams(window.location.search).get('id') || null;
@@ -17,7 +74,7 @@ window.addEventListener('load', async () => {
 
 
     document.getElementById('ProductTitle').value = data.name;
-    document.getElementById('ProductCategory').value = data.categoryName;
+    document.getElementById('ProductCategory').value = data.categoryId;
     document.getElementById('ProductPriority').value = data.priority;
     document.getElementById('ProductPrice').value = data.price;
     tinymce.get("ProductDescription").setContent(data.description);
@@ -25,11 +82,12 @@ window.addEventListener('load', async () => {
     const imagesContainer = document.getElementById('ProductImagesList');
     imagesContainer.innerHTML = "";
 
-    data.images.foreach(img => {
+    data.images.forEach(img => {
         const imageElement = document.createElement("img");
         imageElement.src = `${window.API_BASE_URL}/images/100_${img.name}`;
         imageElement.alt = "Product image";
         imageElement.className = "h-32 rounded shadow";
         imagesContainer.appendChild(imageElement);
     });
+
 });
